6029370bf1b4ee21090ac520c9578b8f6b06ef28
inspiredoff <Vindsf@gmail.com>
2024-11-25 01:26:44 +0300

    created test and refactoring code

--- task2/solution.py
+++ task2/solution.py
@@ -2,7 +2,7 @@ import asyncio
 import aiofiles
 from aiohttp import ClientSession
 from lxml import html
-import confid as conf
+import config as conf
 
 DOMAIN = conf.domain
 PREFIX = conf.prefix
@@ -10,24 +10,24 @@ NAME_FILE = conf.name_file
 RUSSIAN_UPPER_CHARS = conf.russian_upper_chars
 
 
-async def fetch_page(session, url):
+async def fetch_page(session: ClientSession(), url: str) -> str:
     async with session.get(url) as response:
         return await response.text()
 
 
-async def parse_animals(html_text):
+async def parse_animals(html_text: str) -> list[str]:
     makes_html = html.fromstring(html_text)
     animals = [elem.xpath('text()')[0] for elem in makes_html.xpath('//*[@id="mw-pages"]/div[2]/div/div/ul/li/a')]
     return animals
 
 
-async def extract_next_url(html_text):
+async def extract_next_url(html_text: str) -> str:
     makes_html = html.fromstring(html_text)
     next_url = makes_html.xpath('//html/body/div[3]/div[3]/div[5]/div[2]/div[2]/a[2]')[0].get('href')
     return next_url
 
 
-async def count_animals(animals):
+async def count_animals(animals: list[str]) -> dict[str, int]:
     animals_counts = {}
     for animal in animals:
         if animal[0].upper() in RUSSIAN_UPPER_CHARS:
@@ -35,7 +35,7 @@ async def count_animals(animals):
     return animals_counts
 
 
-async def write_to_file(file_name, animal_counts):
+async def write_to_file(file_name: str, animal_counts: dict[str, int]) -> None:
     async with aiofiles.open(file_name, 'w') as f:
         fieldnames = ['Буква', 'Количество']
         await f.write(f'{fieldnames}\n')
@@ -43,7 +43,7 @@ async def write_to_file(file_name, animal_counts):
             await f.write(f'{key},{animal_counts[key]}\n')
 
 
-async def main():
+async def main() -> None:
     animals_counts = {}
     url = DOMAIN + PREFIX
     while url:
