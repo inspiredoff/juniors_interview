1f4e180aed0bdd95ac0020c7765f739a583ca0ec
Inspiredoff <vindsf@gmail.com>
2024-11-27 17:21:07 +0300

    refactored code in data_processor

--- task2/Solution/data_processor.py
+++ task2/Solution/data_processor.py
@@ -1,7 +1,5 @@
 from typing import Any, Dict
 
-from task2.solution import count_animals
-
 
 class AnimalDataProcessor:
 
@@ -10,16 +8,16 @@ class AnimalDataProcessor:
         self.name_animals = pars_data['name_animals']
         self.next_url =domain + pars_data['next_prefix']
         self.name_animal_count = {}
+        await self.__count_animals(self.name_animals)
 
 
-    async def count_animals(self, animals: list[str]) -> Dict[str, int]:
+    async def __count_animals(self, animals: list[str]) -> Dict[str, int]:
         for animal in animals:
             if animal[0].upper() in self.russian_upper_chars:
                 self.name_animal_count[animal[0].upper()] = self.name_animal_count.get(animal[0].upper(), 0) + 1
         return self.name_animal_count
 
     async def parser_data_handler(self) -> Dict[str, int]|None:
-        await self.count_animals(self.name_animals)
         if self.name_animal_count != 0:
             return self.name_animal_count
         else:
