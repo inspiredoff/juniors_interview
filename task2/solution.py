d3f243d15bc1758f9c1853bfcf8e356e38119889
inspiredoff <Vindsf@gmail.com>
2024-11-29 00:49:44 +0300

    refactoring code in task2

--- task2/solution.py
+++ task2/solution.py
@@ -1,4 +1,7 @@
 import asyncio
+from collections import Counter
+from typing import Any
+
 import aiofiles
 from aiohttp import ClientSession
 from lxml import html
@@ -10,28 +13,23 @@ NAME_FILE = conf.name_file
 RUSSIAN_UPPER_CHARS = conf.russian_upper_chars
 
 
-async def fetch_page(session: ClientSession, url: str) -> str:
+async def fetch_page(session: ClientSession, url: str) -> Any:
     async with session.get(url) as response:
-        return await response.text()
+        return html.fromstring(await response.text())
 
 
-async def parse_animals(html_text: str) -> list[str]:
-    makes_html = html.fromstring(html_text)
+async def parse_animals(makes_html: Any) -> list[str]:
     animals = [elem.xpath('text()')[0] for elem in makes_html.xpath('//*[@id="mw-pages"]/div[2]/div/div/ul/li/a')]
     return list(filter(lambda animal: animal[0].upper() in RUSSIAN_UPPER_CHARS, animals))
 
 
-async def extract_next_url(html_text: str, animals: list[str]) -> str:
-    makes_html = html.fromstring(html_text)
+async def extract_next_url(makes_html: str) -> str:
     next_prefix = makes_html.xpath('//html/body/div[3]/div[3]/div[5]/div[2]/div[2]/a[2]')[0].get('href')
-    return next_prefix if len(animals) != 0 else None
+    return next_prefix
 
 
-async def count_animals(animals: list[str], animals_counts: dict[str, int]) -> dict[str, int]:
-    for animal in animals:
-        if animal[0].upper() in RUSSIAN_UPPER_CHARS:
-            animals_counts[animal[0].upper()] = animals_counts.get(animal[0].upper(), 0) + 1
-    return animals_counts
+async def count_animals(animals: list[str]) -> dict[str, int]:
+    return Counter([animal[0].upper() for animal in animals if animal[0].upper() in RUSSIAN_UPPER_CHARS])
 
 
 async def write_to_file(file_name: str, animal_counts: dict[str, int]) -> None:
@@ -48,11 +46,12 @@ async def main() -> None:
     async with ClientSession() as session:
         while url:
             try:
-                html_text = await fetch_page(session, url)
-                animals = await parse_animals(html_text)
-                next_prefix = await extract_next_url(html_text, animals)
-                await count_animals(animals, animals_counts)
-                url = DOMAIN + next_prefix if next_prefix else None
+                make_html = await fetch_page(session, url)
+                animals = await parse_animals(make_html)
+                next_prefix = await extract_next_url(make_html)
+                for key in await count_animals(animals):
+                    animals_counts[key] = animals_counts.get(key, 0) + 1
+                url = DOMAIN + next_prefix
             except Exception as e:
                 print(f"Произошла ошибка: {e}")
                 break
